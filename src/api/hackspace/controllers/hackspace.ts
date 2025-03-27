32b6f612c2c3856f34d2deaf6a02f81c8acba138
He4eT <He4eT@oddsquat.org>
2025-03-27 09:51:46 +0100

    Hackspace: init

---
+++ src/api/hackspace/controllers/hackspace.ts
@@ -0,0 +1,7 @@
+/**
+ * hackspace controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::hackspace.hackspace');
---
+++ src/api/hackspace/routes/hackspace.ts
@@ -0,0 +1,7 @@
+/**
+ * hackspace router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::hackspace.hackspace');
